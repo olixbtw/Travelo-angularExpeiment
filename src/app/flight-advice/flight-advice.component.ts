0ca1c358a854834cd8170ec4b0aa9e662bd1b955
Oleh Lemeshenko <oleh_lemeshenko@epam.com>
2019-10-16 13:42:38 +0300

    created basic sctructure of app. Create components for home page

---
+++ src/app/flight-advice/flight-advice.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-flight-advice',
+  templateUrl: './flight-advice.component.html',
+  styleUrls: ['./flight-advice.component.scss']
+})
+export class FlightAdviceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
