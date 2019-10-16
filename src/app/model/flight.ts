d220e4da9a7415d785c349c57d80b2d605e8a9d8
Oleksandr <oleksandr_revchuk@epam.com>
2019-10-16 12:50:23 +0300

    assets + data models draft added

---
+++ src/app/model/flight.ts
@@ -0,0 +1,36 @@
+export class User {
+  // tslint:disable-next-line: variable-name
+  _id: string;
+  duration: string; // '10H20M';
+  cost: {
+    value: number;
+    currency: string; // 'USD'
+  };
+  destinations: {
+    departure: {
+      city: string; // 'Kyiv'
+      airport: string; // 'KYI'
+      terminal: string;
+      time: 'date';
+    };
+    arrival: {
+      city: string;
+      airport: string;
+      terminal: string;
+      time: 'date'
+    };
+  };
+  stops: {
+    list: [
+      {
+        city: string;
+        airport: string;
+        terminal: string;
+        duration: string;
+        arrival: 'date';
+        departure: 'date'
+      }
+    ];
+  };
+
+}
