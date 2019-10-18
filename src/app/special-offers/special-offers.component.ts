483c73950ea03e3a0bbdb8baf0eb8bbe6f2f3700
Anatolii Shyrakian <anatolii_shyrakian@epam.com>
2019-10-18 16:22:26 +0300

    special-offers component is done

--- src/app/special-offers/special-offers.component.ts
+++ src/app/special-offers/special-offers.component.ts
@@ -7,6 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SpecialOffersComponent implements OnInit {
 
+  public offersTitle: string = 'Special Offers';
+
+  public offers = [
+    {
+      img: '/assets/images/airplane-parking.jpg',
+      alt: 'Airplane parking',
+      title: 'American Airlines are on sale!',
+      description: 'Use out best fare finder to book your journey to Amsterdam on lufthansa.com and save hard cash. In order to find the best-value flights, simply use out best fare finder. Choose your flight to Amsterdam AMS from a wide range of offers.'
+    },
+    {
+      img: '/assets/images/airplane-parking-2.jpg',
+      alt: 'Airplane parking',
+      title: 'Lufthansa is on sale!',
+      description: 'Use out best fare finder to book your journey to Amsterdam on lufthansa.com and save hard cash. In order to find the best-value flights, simply use out best fare finder. Choose your flight to Amsterdam AMS from a wide range of offers.'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
