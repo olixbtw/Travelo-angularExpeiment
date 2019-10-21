a1a5d5778d477a095fbb1d08f9fc95e5b2de8162
Oleksandr <oleksandr_revchuk@epam.com>
2019-10-21 16:31:31 +0300

    minimal working list done

--- src/data/mock/fligths-component.mock.js
+++ src/data/mock/fligths-component.mock.js
@@ -28,11 +28,21 @@ module.exports = [
     totalCost: '$300',
     flights: [
       {
-        departure: 'Kyiv',
-        arrival: 'Paris'
+        departure: { city: 'Kyiv', time: '11:45' },
+        arrival: { city: 'Paris', time: '17:45' },
+        date: 'Wed, Sept 21',
+        provider: {
+          logo: 'companies/lufthansa.png',
+          name: 'Air France'
+        }
       }, {
-        departure: 'Paris',
-        arrival: 'Kyiv'
+        departure: { city: 'Paris', time: '11:45' },
+        arrival: { city: 'Kyiv', time: '17:45' },
+        date: 'Wed, Sept 21',
+        provider: {
+          logo: 'companies/lufthansa.png',
+          name: 'Lufthansa International Airlines'
+        }
       }
     ]
   }, {
@@ -42,11 +52,21 @@ module.exports = [
     totalCost: '$200',
     flights: [
       {
-        departure: 'Athens',
-        arrival: 'Riga'
+        departure: { city: 'Athens', time: '11:45' },
+        arrival: { city: 'Riga', time: '17:45' },
+        date: 'Wed, Sept 21',
+        provider: {
+          logo: 'companies/lufthansa.png',
+          name: 'Air France'
+        }
       }, {
-        departure: 'Riga',
-        arrival: 'Athens'
+        departure: { city: 'Riga', time: '11:45' },
+        arrival: { city: 'Athens', time: '17:45' },
+        date: 'Wed, Sept 21',
+        provider: {
+          logo: 'companies/lufthansa.png',
+          name: 'Air France'
+        }
       }
     ]
   }, {
@@ -56,8 +76,13 @@ module.exports = [
     totalCost: '$100',
     flights: [
       {
-        departure: 'Athens',
-        arrival: 'Riga'
+        departure: { city: 'Athens', time: '11:45' },
+        arrival: { city: 'Riga', time: '17:45' },
+        date: 'Wed, Sept 21',
+        provider: {
+          logo: 'companies/lufthansa.png',
+          name: 'Air France'
+        }
       }
     ]
   }
