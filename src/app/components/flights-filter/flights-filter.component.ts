c31a19774607ec6484a468e6c40c09ffd49b3907
Anatolii Shyrakian <anatolii_shyrakian@epam.com>
2019-10-22 14:40:16 +0300

    Added flights-component

---
+++ src/app/components/flights-filter/flights-filter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-flights-filter',
+  templateUrl: './flights-filter.component.html',
+  styleUrls: ['./flights-filter.component.scss']
+})
+export class FlightsFilterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
