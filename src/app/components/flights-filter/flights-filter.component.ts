f13751f3d2fab18dec6c03d90034afcff9549479
Anatolii Shyrakian <anatolii_shyrakian@epam.com>
2019-10-22 16:24:36 +0300

    Added flights-filter component, adjusting two point range

--- src/app/components/flights-filter/flights-filter.component.ts
+++ src/app/components/flights-filter/flights-filter.component.ts
@@ -13,3 +13,37 @@ export class FlightsFilterComponent implements OnInit {
   }
 
 }
+
+function getVals() {
+  // Get slider values
+  let parent = this.parentNode;
+  let slides = parent.getElementsByTagName('input');
+    let slide1 = parseFloat( slides[0].value );
+    let slide2 = parseFloat( slides[1].value );
+  // Neither slider will clip the other, so make sure we determine which is larger
+  if (slide1 > slide2) {
+    let tmp = slide2; slide2 = slide1; slide1 = tmp;
+  }
+
+  // let displayElement = parent.getElementsByClassName('rangeValues')[0];
+  let valueFrom = document.getElementsByClassName('valueFrom')[0];
+  let valueTo = document.getElementsByClassName('valueTo')[0];
+  	  valueFrom.innerHTML = slide1;
+  	  valueTo.innerHTML = slide2;
+      // displayElement.innerHTML = slide1 + ' - ' + slide2;
+}
+
+window.onload = function() {
+  // Initialize Sliders
+  let sliderSections = document.getElementsByClassName('range-slider');
+      for (let x = 0; x < sliderSections.length; x++) {
+        let sliders = sliderSections[x].getElementsByTagName('input');
+        for (let y = 0; y < sliders.length; y++) {
+        if ( sliders[y].type === 'range') {
+            sliders[y].oninput = getVals;
+            // Manually trigger event first time to display values
+            sliders[y].oninput();
+          }
+        }
+      }
+}
