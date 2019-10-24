f27dcbdd146e582fd53477edb4af08fb002f8677
Anatolii Shyrakian <anatolii_shyrakian@epam.com>
2019-10-24 16:24:43 +0300

    Two point range work with bug

--- src/app/components/flights-filter/flights-filter.component.ts
+++ src/app/components/flights-filter/flights-filter.component.ts
@@ -11,48 +11,16 @@ export class FlightsFilterComponent implements OnInit {
   public minRange: number = 0;
   public maxRange: number = 200;
 
-  public minPoint: number = 0;
-  public maxPoint: number = 200;
+  public minValue = 1;
+  public maxValue = 199;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
-
-    const sliderSections = document.getElementsByClassName('range-slider');
-    for (let x = 0; x < sliderSections.length; x++) {
-      const sliders = sliderSections[x].getElementsByTagName('input');
-      for (let y = 0; y < sliders.length; y++) {
-        if ( sliders[y].type === 'range') {
-          sliders[y].oninput = this.getVals;
-          // Manually trigger event first time to display values
-          sliders[y].oninput();
-        }
-      }
-    }
-
   }
 
-  getVals() {
-    // Get slider values
-    // const parent = this.parentNode;
-    // const slides = parent.getElementsByTagName('input');
-    // let slide1 = slides[0].value;
-    // let slide2 = slides[1].value;
-    let slide1 = 0;
-    let slide2 = 230;
-    // console.log(slide1, slide2, 'What?', this.maxPoint);
-
-    // Neither slider will clip the other, so make sure we determine which is larger
-    if (slide1 > slide2) {
-    	console.log(typeof slide1, typeof slide2);
-      const tmp = slide2; slide2 = slide1; slide1 = tmp;
-    }
-
-    const valueFrom = document.getElementsByClassName('valueFrom')[0];
-    const valueTo = document.getElementsByClassName('valueTo')[0];
-    valueFrom.innerHTML = slide1;
-    valueTo.innerHTML = slide2;
-  }
+  
 
 }
 
