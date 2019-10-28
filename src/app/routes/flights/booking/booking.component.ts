fdce8a8a2ba080cb9b1ee3d38f937dce114eb5b9
Oleksandr <oleksandr_revchuk@epam.com>
2019-10-28 12:51:53 +0200

    changed and removed reused omponents

--- src/app/routes/flights/booking/booking.component.ts
+++ src/app/routes/flights/booking/booking.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormControl } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-booking',
   templateUrl: './booking.component.html',
   styleUrls: ['./booking.component.scss']
 })
 export class BookingComponent implements OnInit {
+  bookingForm = new FormGroup({
+    firstName: new FormControl(),
+    lastName: new FormControl(),
+    country: new FormControl(),
+    sex: new FormControl(),
+    bday: new FormControl(),
+    passportId: new FormControl(),
+    expirydate: new FormControl(),
+    luggage: new FormControl(),
+  });
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+  ) { }
 
-  ngOnInit() {
+  submitUserInfo() {
+    this.router.navigate(['./payment'], { relativeTo: this.route });
   }
 
+  ngOnInit() { }
 }
--- src/app/routes/flights/payment/payment.component.ts
+++ src/app/routes/flights/payment/payment.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-payment',
   templateUrl: './payment.component.html',
   styleUrls: ['./payment.component.scss']
 })
+
 export class PaymentComponent implements OnInit {
 
-  constructor() { }
+  cardForm = new FormGroup({
+    cardNumber: new FormControl(),
+    endDate: new FormControl(),
+    cv2: new FormControl(),
+    cardOwner: new FormControl(),
+  });
 
-  ngOnInit() {
+  submitPayment() {
+    console.log(this.cardForm.value);
+    this.router.navigate(['./trips']);
   }
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+  ) { }
+
+  ngOnInit() { }
 
 }
